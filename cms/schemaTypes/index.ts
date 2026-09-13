2a5c77c08f48c5d6ae948fcb8f4cb56e2a766c04
vitacerta <vitacerta67@gmail.com>
2026-09-13 00:11:43 -0300

    Remove schema de podcast demonstrativo do Sanity

--- cms/schemaTypes/index.ts
+++ cms/schemaTypes/index.ts
@@ -2,6 +2,5 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
-import podcast from './podcast'
 
-export const schemaTypes = [post, podcast, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent]
